1ddee6770ae2f32140cdea617832a595ac6f1611
honeybee <tastynice@foxmail.com>
2019-10-28 10:35:27 +0800

    first

---
+++ src/main/java/com/suilian/App.java
@@ -0,0 +1,13 @@
+package com.suilian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        UserEntity userEntity = new UserEntity();
+        System.out.println(userEntity);
+        System.out.println( "Hello World!" );
+    }
+}
